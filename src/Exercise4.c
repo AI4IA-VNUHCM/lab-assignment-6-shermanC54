d361e9eee823d3784cf2d4cab918d3415a38fbe5
shermanC54 <ITITIU19007@student.hcmiu.edu.vn>
2021-11-22 18:55:28 +0700

    Update Exercise4.c

--- src/Exercise4.c
+++ src/Exercise4.c
@@ -15,7 +15,12 @@ Ex:
 
 void Ex4(char *str){
 	//Your codes here
-	
+	int i, x;
+  	for(i=x=0; str[i]; ++i)
+    if(!isspace(str[i]) || (i > 0 && !isspace(str[i-1])))
+    str[x++] = str[i];
+  	str[x] = '\0';
+	printf("%s\n",str);
 }
 
 int main(int argc, char *argv[]) {
