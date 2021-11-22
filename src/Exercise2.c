7f362d6d11a55ecfddddb1a890e9e9183c97d021
shermanC54 <ITITIU19007@student.hcmiu.edu.vn>
2021-11-22 14:56:02 +0700

    lab06

--- src/Exercise2.c
+++ src/Exercise2.c
@@ -14,7 +14,33 @@ Ex:
 
 void Ex2(int n, char *str[]){
 	//Your codes here
-	
+	int order[n];
+	for(int i = 0; i < n; i++){
+		order[i] = i;
+	}
+	for(int i = 0; i < n; i++){
+		for(int j = i + 1; j < n; j++)
+		{
+			int a = 0;
+
+			while(str[order[i]][a] != '\0')
+			{
+				if(str[order[i]][a] > str[order[j]][a])
+				{
+					int temp = order[i];
+					order[i] = order[j];
+					order[j] = temp;
+					break;
+				}else if(str[order[i]][a] == str[order[j]][a]) 	
+					a++;
+				else
+					break;
+			}
+		}
+	}
+	for(int i = 0; i < n; i++){
+		printf("%s ", str[order[i]]);
+	}
 }
 
 int main(int argc, char *argv[]) {
